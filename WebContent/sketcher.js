582aae2bd1544df295882f718c8c22e72229c80f
Chris Greenhalgh <cmg@cs.nott.ac.uk>
2012-07-20 23:06:00 +0100

    zoom in progress (paperjs bugs), starting text

--- WebContent/sketcher.js
+++ WebContent/sketcher.js
@@ -1,3 +1,10 @@
+// Note, problems with paperjs versions.
+// Nightly build recent (July 2012) throws an exception early on in the View initialisation
+// with view apparently null.
+// Stable release 0.2.2 has (at least) a bug in the way the centre is updated when zooming. See
+// NB: https://github.com/eric-wieser/paper.js/tree/patch-1
+// "Fixed bad centering when `view.center` and `view.zoom` are changed"
+
 // redraw all views 
 function redraw(ps) {
 	for (var vi in ps.views) {
@@ -75,6 +82,7 @@ var paperScopes = new Array();
 function setupOverviewAndDetail(overviewId, detailId) {
 
 	var ps = new paper.PaperScope();
+	paper = ps;
     paperScopes.push(ps);
     
     var overviewCanvas = document.getElementById(overviewId);
@@ -181,12 +189,18 @@ function isSpecialKey(which) {
 	}
 }
 
+// common mouse/key interaction state
 var mouseTarget = undefined;
 var mousePageX = undefined;
 var mousePageY = undefined;
 var keyTarget = undefined;
 var keyDown = undefined;
 
+/** get paperjs view for event target iff it is a canvas.
+ * also initialises global paper if found.
+ * 
+ * @return View if found, else null
+ */ 
 function getView(target) {
 	for (var psi in paperScopes) {
 		var ps = paperScopes[psi];
@@ -205,32 +219,173 @@ function getView(target) {
 	return null;
 }
 
-var aTool = new Object();
-var aToolPath = undefined;
+/** line tool */
+var lineTool = new Object();
+var lineToolPath = undefined;
 
-aTool.begin = function(point) {
-	console.log('aTool.begin '+point);
+lineTool.begin = function(point) {
+	console.log('lineTool.begin '+point);
 	
-	aToolPath = new paper.Path();
-	aToolPath.strokeColor = 'black';
-	aToolPath.add(point);	
+	lineToolPath = new paper.Path();
+	lineToolPath.strokeColor = 'black';
+	lineToolPath.add(point);	
 };
-aTool.move = function(point) {
-	//console.log('aTool.move '+point);
+lineTool.move = function(point) {
+	//console.log('lineTool.move '+point);
 
-	aToolPath.add(point);
+	lineToolPath.add(point);
 };
-aTool.end = function(point) {
-	console.log('aTool.end'+point);
+lineTool.end = function(point) {
+	console.log('lineTool.end'+point);
 
-	aToolPath.simplify();
-	aToolPath = undefined;
+	lineToolPath.simplify();
+	lineToolPath = undefined;
 };
 
+/** convert CSS points to screen pixels, see http://stackoverflow.com/questions/279749/detecting-the-system-dpi-ppi-from-js-css */
+function pt2px(pt) {
+	screenPPI = document.getElementById('ppitest').offsetWidth;
+	return pt*screenPPI/72;
+}
+
+/** workaround for editable multi-line text */
+function createTextBlock(point, lines, justification) {
+	if (justification===undefined)
+		justification = 'center';
+	var characterStyle = { fillColor: 'black', fontSize: 12 };
+	var lineSpacing = pt2px(characterStyle.fontSize)*1.15;
+	var block = new paper.Group();
+	for (var li in lines) {
+		var line = lines[li];
+		var text = new paper.PointText(new paper.Point(point.x, point.y));
+		text.content = line;
+		text.paragraphStyle.justification = justification;
+		text.characterStyle = characterStyle;
+		block.addChild(text);
+		point.y = point.y + lineSpacing;
+	}
+}
+
+/** text tool */
+var textTool = new Object();
+var textToolOutline = undefined;
+var textToolText = undefined;
+var textToolBegin = undefined;
+var textToolPos = 0;
+
+textTool.begin = function(point) {
+	console.log('textTool.begin '+point);
+	textToolBegin = point;
+};
+textTool.move = function(point) {
+	//console.log('lineTool.move '+point);
+	if (textToolOutline)
+		textToolOutline.remove();
+	textToolOutline = new paper.Path.Rectangle(textToolBegin, point);
+	textToolOutline.strokeColor = 'grey';
+	textToolOutline.dashArray = [10,4];
+};
+textTool.end = function(point) {
+	console.log('textTool.end'+point);
+	if (textToolOutline) {
+		textToolOutline.remove();
+		textToolOutline = undefined;
+	}
+	var width = Math.abs(point.x-textToolBegin.x);
+	var x = (point.x+textToolBegin.x)/2;
+	var y = (point.y+textToolBegin.y)/2;
+	textToolBegin = undefined;
+	createTextBlock(new paper.Point(x, y), ['testing','testing','1, 2, 3...']);
+};
+
+/** zoom in tool */
+var zoomInTool = new Object();
+var zoomOutTool = new Object();
+var ZOOM_INTERVAL = 20;
+var ZOOM_RATIO = 0.02;
+var zoomInterval = undefined;
+var zoomPoint = undefined;
+var zoomView = undefined;
+
+function zoomIn() {
+	if (!zoomView || !zoomPoint)
+		return;
+	// zoom towards zoomPoint in project space
+	var zoom = zoomView.zoom;
+	console.log('zoomIn point='+zoomPoint+' zoom='+zoomView.zoom+' center='+zoomView.center);
+	zoomView.zoom = zoomView.zoom*(1+ZOOM_RATIO);
+	var dx = zoomPoint.x-zoomView.center.x;
+	var dy = zoomPoint.y-zoomView.center.y;
+	var sdx = (ZOOM_RATIO*dx*zoom)/zoomView.zoom;
+	var sdy = (ZOOM_RATIO*dy*zoom)/zoomView.zoom;
+	console.log('- d='+dx+','+dy+' zoom\'='+zoomView.zoom+' sd='+sdx+','+sdy);
+	zoomView.center = new paper.Point(zoomView.center.x+sdx, zoomView.center.y+sdy);
+	console.log('- center\'='+zoomView.center);
+}
+
+function zoomOut() {
+	if (!zoomView || !zoomPoint)
+		return;
+	// zoom away from zoomPoint in project space
+	console.log('zoomOut point='+zoomPoint+' zoom='+zoomView.zoom+' center='+zoomView.center);
+	var zoom = zoomView.zoom;
+	zoomView.zoom = zoomView.zoom*(1-ZOOM_RATIO);
+	var dx = zoomPoint.x-zoomView.center.x;
+	var dy = zoomPoint.y-zoomView.center.y;
+	var sdx = (ZOOM_RATIO*dx*zoom)/zoomView.zoom;
+	var sdy = (ZOOM_RATIO*dy*zoom)/zoomView.zoom;
+	console.log('- d='+dx+','+dy+' zoom\'='+zoomView.zoom+' sd='+sdx+','+sdy);
+	zoomView.center = new paper.Point(zoomView.center.x-sdx, zoomView.center.y-sdy);
+	console.log('- center\'='+zoomView.center);
+}
+
+zoomInTool.begin = function(point, view) {
+	zoomPoint = point;
+	zoomView = view;
+	zoomInterval = setInterval(zoomIn, ZOOM_INTERVAL);
+	zoomIn();
+};
+zoomOutTool.begin = function(point, view) {
+	zoomPoint = point;
+	zoomView = view;
+	zoomInterval = setInterval(zoomOut, ZOOM_INTERVAL);
+	zoomOut();
+};
+zoomOutTool.move = zoomInTool.move = function(point) {
+	zoomPoint = point;	
+};
+zoomOutTool.end = zoomInTool.end = function(point) {
+	zoomPoint = point;		
+	clearInterval(zoomInterval);
+	zoomView = undefined;
+};
+
+/** get key string for code */
+function getToolKeyWhich(which) {
+	return 'code'+which;
+}
+/** get key string for char */
+function getToolKeyChar(c) {
+	return 'code'+c.charCodeAt(0);
+}
+
+/** global - all tools keys by key */
 var tools = new Object();
-tools['code'+('A'.charCodeAt(0))] = aTool;
+tools[getToolKeyChar('A')] = lineTool;
+tools[getToolKeyChar('S')] = textTool;
+tools[getToolKeyChar('Z')] = zoomInTool;
+tools[getToolKeyChar('X')] = zoomOutTool;
 
 var tool = undefined;
+var toolView = undefined;
+
+/** function to map view pixel position to project coordinates.
+ * @return Point */
+function view2project(view, vx, vy) {
+	var px = (vx-view.canvas.width/2)/view.zoom+view.center.x;
+	var py = (vy-view.canvas.height/2)/view.zoom+view.center.y;
+	return new paper.Point(px, py);
+}
 
 // Only executed our code once the DOM is ready.
 $(document).ready(function() {
@@ -249,18 +404,19 @@ $(document).ready(function() {
 
 		if (tool) {
 			// switch tool
-			tool.end(new paper.Point(mousePageX-keyTarget.offsetLeft, mousePageY-keyTarget.offsetTop));
+			tool.end(view2project(toolView, mousePageX-keyTarget.offsetLeft, mousePageY-keyTarget.offsetTop));
 			tool = undefined;
 			redraw(paper);
 		}
 		keyTarget = mouseTarget;
 		var v = getView(keyTarget);
 		if (v) {
-			var t = tools['code'+ev.which];
+			var t = tools[getToolKeyWhich(ev.which)];
 			if (t) {
 				console.log('begin tool '+t);
 				tool = t;
-				tool.begin(new paper.Point(mousePageX-keyTarget.offsetLeft, mousePageY-keyTarget.offsetTop));
+				toolView = v;
+				tool.begin(view2project(toolView, mousePageX-keyTarget.offsetLeft, mousePageY-keyTarget.offsetTop), v);
 				redraw(paper);
 			}
 		}
@@ -275,7 +431,7 @@ $(document).ready(function() {
 		keyDown = undefined;
 		if (tool) {
 			// switch tool
-			tool.end(new paper.Point(mousePageX-keyTarget.offsetLeft, mousePageY-keyTarget.offsetTop));
+			tool.end(view2project(toolView, mousePageX-keyTarget.offsetLeft, mousePageY-keyTarget.offsetTop));
 			redraw(paper);
 			tool = undefined;
 		}
@@ -303,7 +459,7 @@ $(document).ready(function() {
 		mousePageX = ev.pageX;
 		mousePageY = ev.pageY;
 		if (tool) {
-			tool.move(new paper.Point(mousePageX-keyTarget.offsetLeft, mousePageY-keyTarget.offsetTop));
+			tool.move(view2project(toolView, mousePageX-keyTarget.offsetLeft, mousePageY-keyTarget.offsetTop));
 			redraw(paper);
 		}
 	});
@@ -312,7 +468,7 @@ $(document).ready(function() {
 		console.log('mousedown: '+ev.which+' at '+ev.pageX+','+ev.pageY+' on '+ev.target+' = '+(ev.pageX-ev.target.offsetLeft)+','+(ev.pageY-ev.target.offsetTop));
 		if (tool) {
 			// switch tool
-			tool.end(new paper.Point(mousePageX-keyTarget.offsetLeft, mousePageY-keyTarget.offsetTop));
+			tool.end(view2project(toolView, mousePageX-keyTarget.offsetLeft, mousePageY-keyTarget.offsetTop));
 			redraw(paper);
 			tool = undefined;
 		}
